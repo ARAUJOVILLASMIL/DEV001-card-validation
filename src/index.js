31c9ed2ee27c77384c0f4696761af3902810cdca
Adriana Araujo <araujovillasmil@gmail.com>
2022-10-03 13:12:20 -0300

    preparar codigo para enviarlo a github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
-console.log(validator);
+const validar = document.getElementById("validar")
+ validar.addEventListener("click",capturarnumero)
+ validar.addEventListener("click",mascara)
+
+// funcion que captura el numero que ingresa el usuario
+// vlue, es el valor que ingresa o numero de tarjeta
+function capturarnumero(){
+ const numero = document.getElementById("numero").value
+
+// esta funcion valida el numero que el usuario ingresa.
+ let valido=validator.isValid(numero)
+if (valido){
+    alert ("es valido")    
+}
+else {
+    alert("no es valido")
+}
+
+}
+//esta funcion es la que va tapar los numeros enmascarar
+ function mascara(){
+   const inputtarjeta = document.getElementById("numero").value
+   let mascarilla = validator.maskify(inputtarjeta) 
+   document.getElementById("numero").value =mascarilla
+ }
+
+
